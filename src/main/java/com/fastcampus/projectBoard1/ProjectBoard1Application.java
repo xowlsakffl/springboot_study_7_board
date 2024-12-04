29494b229a4f4eb9c3eb09542cc5f5141cdff9fd
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-12-04 23:29:53 +0900

    #1 환경 세팅 완료

---
+++ src/main/java/com/fastcampus/projectBoard1/ProjectBoard1Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectBoard1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoard1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBoard1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/projectBoard1/ProjectBoard1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectBoard1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoard1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
