097e46639ffeec530f3b7b9272bc9e58a170ee90
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-12-06 00:28:14 +0900

    #2 뷰,페이징-1

---
+++ src/test/java/com/fastcampus/projectBoard1/controller/ArticleControllerTest.java
@@ -0,0 +1,54 @@
+package com.fastcampus.projectBoard1.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class) // 이 컨트롤러만 해당
+class ArticleControllerTest {
+    private final MockMvc mvc;
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트")
+    @Test
+    public void givenNothing_whenRequestArticlesView_thenReturnArticlesView() throws Exception {
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지")
+    @Test
+    public void givenNothing_whenRequestArticleView_thenReturnArticleView() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
+        mvc.perform(get("/articles/serarch"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 전용 페이지")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
+        mvc.perform(get("/articles/serarch-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
