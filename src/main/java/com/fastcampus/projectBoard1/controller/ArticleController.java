58744c3aee455f10e272f30e0d36ff64b8272685
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-12-16 16:59:53 +0900

    #8 페이지 기능 구현-3

--- src/main/java/com/fastcampus/projectBoard1/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectBoard1/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.fastcampus.projectBoard1.domain.type.SearchType;
 import com.fastcampus.projectBoard1.dto.response.ArticleResponse;
 import com.fastcampus.projectBoard1.dto.response.ArticleWithCommentResponse;
 import com.fastcampus.projectBoard1.service.ArticleService;
+import com.fastcampus.projectBoard1.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -22,6 +24,7 @@ import java.util.List;
 @Controller
 public class ArticleController {
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -30,7 +33,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
