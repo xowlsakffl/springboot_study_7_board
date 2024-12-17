d4a6c02f140ba0648f88a4ebab3e2107e412457b
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-12-18 00:28:28 +0900

    #9 게시판 정렬 기능

--- src/main/java/com/fastcampus/projectBoard1/config/ThymeleafConfig.java
+++ src/main/java/com/fastcampus/projectBoard1/config/ThymeleafConfig.java
@@ -5,11 +5,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
-public class ThymeleadConfig {
+public class ThymeleafConfig {
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
             SpringResourceTemplateResolver defaultTemplateResolver,
--- src/main/java/com/fastcampus/projectBoard1/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectBoard1/domain/UserAccount.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 @Getter
 @ToString(callSuper = true)
 @Table(indexes = {
+        @Index(columnList = "userId"),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
