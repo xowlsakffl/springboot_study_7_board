68885b6b8aabd5d3789ddf25b249d33c713c8aa8
msan <50791439+xowlsakffl@users.noreply.github.com>
2024-12-27 18:18:35 +0900

    [고도화] 테스트 수정

--- src/test/java/com/fastcampus/projectBoard1/ProjectBoard1ApplicationTests.java
+++ src/test/java/com/fastcampus/projectBoard1/ProjectBoard1ApplicationTests.java
@@ -2,7 +2,9 @@ package com.fastcampus.projectBoard1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ProjectBoard1ApplicationTests {
 
