edc1f0459e0b69b05a2592bbda9a9cb2d9df04c3
msan <50791439+xowlsakffl@users.noreply.github.com>
2025-01-08 06:13:13 +0900

    DataRestConfig 추가

---
+++ src/main/java/com/fastcampus/projectBoard1/config/DataRestConfig.java
@@ -0,0 +1,25 @@
+package com.fastcampus.projectBoard1.config;
+
+import com.fastcampus.projectBoard1.domain.Article;
+import com.fastcampus.projectBoard1.domain.ArticleComment;
+import com.fastcampus.projectBoard1.domain.Hashtag;
+import com.fastcampus.projectBoard1.domain.UserAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return RepositoryRestConfigurer.withConfig((config, cors) ->
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
+        );
+    }
+
+}
